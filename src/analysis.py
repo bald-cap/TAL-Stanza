1edf26d82849102d3e159f66865d847560851437
bessy-carelle <bessycarel02@gmail.com>
2025-01-09 11:32:33 +0100

    segmentation en tokens fini

--- src/analysis.py
+++ src/analysis.py
@@ -1,3 +1,20 @@
 # %%
-print("Hello")
-# %%
+import stanza
+nlpStanza=stanza.Pipeline("fr")
+#%%
+"""Segmentation en tokens du fichier textProjet.txt"""
+#%%
+with open("textProjet.txt","r",encoding="utf-8") as fichier:
+    texte = fichier.read()
+    st = nlpStanza(texte)
+    print ("\n\tSTANZA\n---")
+    count = 0
+    total_mot = 0
+    for sent in st.sentences:
+        count += 1
+        print(f"------\n\nPHRASE {count} -> {sent.text}", end="\n------\n")
+        count_mot = 0
+        for x in sent.tokens:
+            count_mot += 1
+            print(f"{count_mot} - {x.text}", end="\n")
+#%%
